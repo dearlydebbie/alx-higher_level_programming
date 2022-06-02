0ce2d096ee8af9ba4546939f20c34aaaa119ff2b
Deborah Adeoye <debbieadeoye1@gmail.com>
2022-06-02 12:30:03 +0100

    pycodestyle

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,6 @@
-from add_0 import add
-
-
-def main():
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
-
-
-if __name__ == "__main__":
-    main()
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
